35dfd3f01e289bf1f6e500711559568e5a628dd8
SantinoGiordano <santinogiordano13@gmail.com>
2025-02-04 12:34:44 -0500

    added a classname for home

--- reactFrontend/src/home.tsx
+++ reactFrontend/src/home.tsx
@@ -1,11 +1,13 @@
+import UserForm from "./componets/userForm";
 import UserList from "./componets/userList";
 
 export default function Home() {
   return (
     <>
-      <div>
+      <div className="">
         Hello
         <UserList />
+        <UserForm />
       </div>
     </>
   );
